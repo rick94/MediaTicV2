fc3bd924ebc880432ffea17ce596072d57586150
Esteban Castillo <steban.94@hotmail.com>
2018-02-16 22:09:28 -0600

    Added function to import multiple gdf files to Neo4j

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # GDF importer to Neo4J
 This script reads the data from gdf files and inserts it in a Neo4J database.
+
+### How to use:
+Create the directory `imports` inside the directory where you are running the script
+and copy your gdf files there.
--- test.py
+++ test.py
@@ -1,18 +1,24 @@
 from GDF_importer import parseGDF
 from GdfToNeo import importGDFToNeo
 from neo4j.v1 import GraphDatabase, basic_auth #to connect to the database
+import os, errno
+
 
 driver = GraphDatabase.driver("bolt://localhost:7687", auth=basic_auth("neo4j", "neo4j"))
 session = driver.session()
 
-elements = parseGDF("sample.gdf")
-query_list = importGDFToNeo(elements)
-
-queryNumber = len(query_list)
-queryCounter = 0
-for query in query_list:
-    session.run(query)
-    queryCounter += 1
-    print(queryCounter,' de ',queryNumber,' consultas ejecutadas')
+gdf_files_directory = os.path.dirname(os.path.abspath(__file__)) + '\imports'
+for path, dirs, files in os.walk(gdf_files_directory):
+    for file in os.listdir(path):
+        if file.endswith('.gdf'):
+            gdf_file = os.path.join(path, file)
+            elements = parseGDF(gdf_file)
+            query_list = importGDFToNeo(elements)
+            queryNumber = len(query_list)
+            queryCounter = 0
+            for query in query_list:
+                session.run(query)
+                queryCounter += 1
+                print(queryCounter,' de ',queryNumber,' consultas ejecutadas')
 
 session.close()
